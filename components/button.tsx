eca20d8a2716938faeff026b961fe643bc01a5f8
Kingsley <kingsleymuturi9@gmail.com>
2022-03-27 05:01:03 +0300

    fix ripple animation and other styles

--- components/button.tsx
+++ components/button.tsx
@@ -10,15 +10,16 @@ interface ButtonProps{
 export function Button({beginPlay, choice, result, disable} : ButtonProps){
     return(
         <div className={styles.buttonContainer}>
-            <button disabled = {disable} type="button"
-                className={
-                    choice === 'rock'? styles.rock:
-                    choice === 'paper'? styles.paper:
-                    styles.scissors
-                }
-                onClick={() => beginPlay(choice)}>
-                <img src={`/icon-${choice}.svg`}/>
-            </button>
+                <button disabled = {disable} type="button"
+                    className={
+                        choice === 'rock'? styles.rock:
+                        choice === 'paper'? styles.paper:
+                        styles.scissors
+                    }
+                    onClick={() => beginPlay(choice)}
+                    >
+                    <img src={`/icon-${choice}.svg`}/>
+                </button>
             {result && <span/>}
         </div>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/styles.css';
+import '../styles/Styles.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
